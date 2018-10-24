510efa2ba3d8a24bbbee035f7a15912804d25ee8
Anders Weiskvist <Weiskvist94@gmail.com>
2018-10-24 16:17:45 +0200

    Rounds er done!

--- TournamentLibrary/Round.cs
+++ TournamentLibrary/Round.cs
@@ -6,6 +6,7 @@ namespace TournamentLib
     {
         private List<Match> matches = new List<Match>();
         
+
         public void AddMatch(Match m)
         {
             matches.Add(m);
@@ -14,25 +15,65 @@ namespace TournamentLib
         public Match GetMatch(string teamName1, string teamName2)
         {
             // TODO: Implement this method
-            return null;
+            Match match = new Match();
+            foreach (Match m in matches)
+            {
+                if (m.FirstOpponent.ToString() == teamName1 && m.SecondOpponent.ToString() == teamName2)
+                {
+                    match = m;
+                }
+                
+            }
+            return match;
         }
 
         public bool IsMatchesFinished()
         {
+            for (int i = 0; i < matches.Count; i++)
+            {
+                if (matches[i].Winner == null)
+                {
+                    return false;
+                }
+                
+            }
             // TODO: Implement this method
-            return false;
+
+            return true;
         }
 
         public List<Team> GetWinningTeams()
         {
+
+            List<Team> WinningTeams = new List<Team>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                WinningTeams.Add(matches[i].Winner);
+            }
+            return WinningTeams;
             // TODO: Implement this method
-            return null;
+            
         }
 
         public List<Team> GetLosingTeams()
         {
+            List<Team> LosingTeams = new List<Team>();
+            List<Team> WinningTeams = new List<Team>();
+            foreach (var match in matches)
+            {
+                if (match.Winner == match.FirstOpponent)
+                {
+                    WinningTeams.Add(match.FirstOpponent);
+                    LosingTeams.Add(match.SecondOpponent);
+                }
+                else
+                {
+                    LosingTeams.Add(match.FirstOpponent);
+                    WinningTeams.Add(match.SecondOpponent);
+                }
+            }
             // TODO: Implement this method
-            return null;
+            return LosingTeams;
         }
     }
 }
