798dc0c597d38b1c497556a36ea39f18a3bf472e
Maria Hovgaard Rasmussen <Mari55fe@edu.eal.dk>
2018-11-15 14:05:15 +0100

    Prøver fjernet igen

--- TournamentLibrary/TournamentRepo.cs
+++ TournamentLibrary/TournamentRepo.cs
@@ -5,15 +5,7 @@ namespace TournamentLib
     public class TournamentRepo
     {
         private List<Tournament> tournaments = new List<Tournament>();
-
-        public string Name { get; private set; }
-
-        public void Tournament(string tournamentName) 
-        
-        {
-            Name = tournamentName;
-        }
-    
+   
                
         public void AddTournament(Tournament name)
         {
